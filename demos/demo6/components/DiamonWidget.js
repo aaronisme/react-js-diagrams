283ab092238c0cb84a239388e8631ee9234b33ed
chen.dong <chen_dong85@foxmail.com>
2017-09-24 14:57:13 +1000

    refactor the widget component

--- demos/demo6/components/DiamonWidget.js
+++ demos/demo6/components/DiamonWidget.js
@@ -1,35 +1,30 @@
 import * as React from 'react';
 import * as SRD from '../../../src/main';
 
-export  default class DiamondWidget extends React.Component {
-  static defaultProps = {
-    size: 150,
-    node: null
-  };
-  
-  render() {
-    return (
-      <div className="diamond-node" style={{position: 'relative', width: this.props.size, height: this.props.size}}>
-        <svg width={this.props.size} height={this.props.size}>
-          <g id="Layer_1"/>
-          <g id="Layer_2">
-            <polygon fill="cyan" stroke="#000000" strokeWidth="3" strokeMiterlimit="10"
-                     points={`10,${this.props.size / 2} ${this.props.size / 2},10 ${this.props.size - 10},${this.props.size / 2} ${this.props.size / 2},${this.props.size - 10}`}/>
-          </g>
-        </svg>
-        <div style={{position: 'absolute', zIndex: 10, top: this.props.size / 2 - 5}}>
-          <SRD.PortWidget name={'node-1'} node={this.props.node}/>
-        </div>
-        <div style={{position: 'absolute', zIndex: 10, left: this.props.size / 2 - 8}}>
-          <SRD.PortWidget name={'node-2'} node={this.props.node}/>
-        </div>
-        <div style={{position: 'absolute', zIndex: 10, left: this.props.size - 10, top: this.props.size / 2}}>
-          <SRD.PortWidget name={'node-3'} node={this.props.node}/>
-        </div>
-        <div style={{position: 'absolute', zIndex: 10, left: this.props.size / 2 - 8, top: this.props.size - 10}}>
-          <SRD.PortWidget name={'node-4'} node={this.props.node}/>
-        </div>
+const DiamondWidget = ({size = 150, node}) => {
+  return (
+    <div className="diamond-node" style={{position: 'relative', width: size, height: size}}>
+      <svg width={size} height={size}>
+        <g id="Layer_1"/>
+        <g id="Layer_2">
+          <polygon fill="cyan" stroke="#000000" strokeWidth="3" strokeMiterlimit="10"
+                   points={`10,${size / 2} ${size / 2},10 ${size - 10},${size / 2} ${size / 2},${size - 10}`}/>
+        </g>
+      </svg>
+      <div style={{position: 'absolute', zIndex: 10, top: size / 2 - 5}}>
+        <SRD.PortWidget name={'node-1'} node={node}/>
       </div>
-    )
-  }
-}
+      <div style={{position: 'absolute', zIndex: 10, left: size / 2 - 8}}>
+        <SRD.PortWidget name={'node-2'} node={node}/>
+      </div>
+      <div style={{position: 'absolute', zIndex: 10, left: size - 10, top: size / 2}}>
+        <SRD.PortWidget name={'node-3'} node={node}/>
+      </div>
+      <div style={{position: 'absolute', zIndex: 10, left: size / 2 - 8, top: size - 10}}>
+        <SRD.PortWidget name={'node-4'} node={node}/>
+      </div>
+    </div>
+  )
+};
+
+export  default DiamondWidget
